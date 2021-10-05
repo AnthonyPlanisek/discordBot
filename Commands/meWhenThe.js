1ac2c4967bc710ba2c73847b60934bbc9751d425
Rupert Foley <rupert.foley@gmail.com>
2021-10-05 16:59:07 +0100

    Me when the when the when when me when when the when when the when when the me me when me the when the when me me when me when when me when me

--- Commands/meWhenThe.js
+++ Commands/meWhenThe.js
@@ -12,10 +12,24 @@ module.exports = new Command({
 })
 
 function MeWhenThe(){
-    let amount = Math.random * 100;
-    let stringToPrint = "Me when the"
+    let amount = Math.round(Math.random() * 50);
+    console.log(amount);
+    let printString = "Me when the";
+    let lastNum = 2;
+    let secondLastNum = 1;
+    let thirdLastNum = 0;
     for (let i = 0; i < amount; i++){
-        temp = Math.random * 3;
+        temp = Math.round(Math.random() * 2);
+        while((lastNum == secondLastNum && lastNum == temp) 
+            ||(thirdLastNum == secondLastNum && lastNum == temp))
+        {
+            temp = Math.round(Math.random() * 2);
+            console.log(temp)
+        }
+        thirdLastNum = secondLastNum;
+        secondLastNum = lastNum;
+        lastNum = temp;
+
         if(temp == 0){
             printString += " me";
         } else if(temp == 1){
@@ -24,5 +38,6 @@ function MeWhenThe(){
             printString += " the";
         }
     }
-    return stringToPrint;
+    return printString;
 }
+
